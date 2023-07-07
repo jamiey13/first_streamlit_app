e9f4e5d0ebf981044ef00f6ec0f159a6f7756f17
jamiey13 <133860099+jamiey13@users.noreply.github.com>
2023-07-07 16:55:11 +0100

    Update streamlit_app.py
    Added pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.title('What is going on here?')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 🥗 🐔 🥑🍞')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
