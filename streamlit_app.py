e30c8bf14cc49ed385619f7d489891f8d945fb5e
jamiey13 <133860099+jamiey13@users.noreply.github.com>
2023-07-07 15:18:30 +0100

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('What is going on here?')
 
-streamlit.header('Menu')
-streamlit.text('I wonder what I will learn on this course?')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 🥗 🐔 🥑🍞')
